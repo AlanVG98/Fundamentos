b460c3ef8003a276f3b968c4baf2f3d4c03a7f60
Alan <alanvizcaino98@gmail.com>
2021-10-04 20:24:06 -0700

    Cadenas de python

---
+++ origin.py
@@ -0,0 +1,18 @@
+name = 'alan giovanni vizcaino'
+age = 23
+istall = True
+height = 1.71
+print ('{} tiene {} años'.format(name,age))
+
+print(name+' tiene'+ str(age)+ ' años')
+
+print(f'{name} tiene {age} años y es\
+alto {istall} y mide {height}')
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name*5)
+
+
+
